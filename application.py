f90802e44516afc796f4e4e8c9a94410ffc7a17a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2024-12-16 15:49:53 +0100

    Update application.py

--- application.py
+++ application.py
@@ -7,6 +7,8 @@ from data import content_json
 #openai.organization = "org-jOS3rcSS5l2406Nw1OF6NZXw"
 #openai.api_key =  "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"
 
+
+
 #response = openai.Completion.create(
 #    engine="davinci", 
 #    prompt= "This is a test",
